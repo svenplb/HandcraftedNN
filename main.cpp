7f288ff4986e46cab01aa6644659d12501b34903
svenplb <psven595@gmail.com>
2024-08-23 15:05:05 +0200

    bias

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@ int classify(double input_1, double input_2);
 double weight_1_1, weight_2_1;
 // Weight values connecting each input to the second output
 double weight_1_2, weight_2_2;
+// Bias | y = k*x+d (d)
+double bias_1, bias_2;
+
 
 int main(void) {
 	double input_1 = 123.123;
@@ -17,8 +20,8 @@ int main(void) {
 }
 
 int classify(double input_1, double input_2) {
-	double output_1 = input_1 * weight_1_1 + input_2 * weight_2_1;
-	double output_2 = input_2 * weight_2_1 + input_2 * weight_2_2;
+	double output_1 = input_1 * weight_1_1 + input_2 * weight_2_1 + bias;
+	double output_2 = input_2 * weight_2_1 + input_2 * weight_2_2 + bias;
 
 	cout << output_1;
 	cout << output_2;
