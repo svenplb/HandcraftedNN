5160898015a26c177f87d87e18516b80518f533d
sven <97550617+svenplb@users.noreply.github.com>
2024-08-27 19:30:26 +0200

    training loop

--- model.cpp
+++ model.cpp
@@ -1,12 +1,19 @@
+// TODO: Better Comments, Clean Up, Use actual data, training.cpp file, refactor
+
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <cmath>
 
 class Layer
 {
 public:
     // on the middle layer of a (2,3,2) neural net nodesIn will be 2, nodesOut will be 3
     int numNodesIn, numNodesOut;
+
+    // cost gradients weights and biases
+    std::vector<std::vector<double>> costGradientWeights;
+    std::vector<double> costGradientBias;
 
     std::vector<std::vector<double>> weights;
     std::vector<double> biases;
@@ -16,9 +23,27 @@ public:
         this->numNodesIn = numNodesIn;
         this->numNodesOut = numNodesOut;
 
+        costGradientWeights = std::vector<std::vector<double>>(numNodesIn, std::vector<double>(numNodesOut, 0.0));
+        costGradientBias = std::vector<double>(numNodesOut, 0.0);
+
         // initialize weights and biases with 0
         weights = std::vector<std::vector<double>>(numNodesIn, std::vector<double>(numNodesOut, 0.0));
         biases = std::vector<double>(numNodesOut, 0.0);
+
+        InitializeRandomWeights();
+    }
+
+    void InitializeRandomWeights()
+    {
+        srand(static_cast<unsigned int>(time(0)));
+        for (int nodeIn = 0; nodeIn < numNodesIn; ++nodeIn)
+        {
+            for (int nodeOut = 0; nodeOut < numNodesOut; ++nodeOut)
+            {
+                double randomValue = static_cast<double>(rand()) / RAND_MAX * 2.0 - 1.0;
+                weights[nodeIn][nodeOut] = randomValue / sqrt(numNodesIn);
+            }
+        }
     }
 
     std::vector<double> CalculateOutputs(const std::vector<double> &inputs)
@@ -51,6 +76,18 @@ public:
     {
         double error = outputActivation - expectedOutput;
         return error * error;
+    }
+
+    void ApplyGradients(double learnRate)
+    {
+        for (int nodeOut = 0; nodeOut < numNodesOut; nodeOut++)
+        {
+            biases[nodeOut] -= costGradientBias[nodeOut] * learnRate;
+            for (int nodeIn = 0; nodeIn < numNodesIn; nodeIn++)
+            {
+                weights[nodeIn][nodeOut] -= costGradientWeights[nodeIn][nodeOut] * learnRate;
+            }
+        }
     }
 };
 
@@ -115,18 +152,76 @@ public:
         }
         return totalCost / inputsList.size();
     }
+
+    void Learn(const std::vector<std::vector<double>> &inputsList, const std::vector<std::vector<double>> &expectedOutputsList, double learnRate)
+    {
+        const double h = 0.0001;
+        double originalCost = Loss(inputsList, expectedOutputsList);
+
+        for (Layer &layer : layers)
+        {
+            for (int nodeIn = 0; nodeIn < layer.numNodesIn; nodeIn++)
+            {
+                for (int nodeOut = 0; nodeOut < layer.numNodesOut; nodeOut++)
+                {
+                    layer.weights[nodeIn][nodeOut] += h;
+                    double deltaCost = Loss(inputsList, expectedOutputsList) - originalCost;
+                    layer.weights[nodeIn][nodeOut] -= h;
+                    layer.costGradientWeights[nodeIn][nodeOut] = deltaCost / h;
+                }
+            }
+
+            for (int biasIndex = 0; biasIndex < layer.biases.size(); biasIndex++)
+            {
+                layer.biases[biasIndex] += h;
+                double deltaCost = Loss(inputsList, expectedOutputsList) - originalCost;
+                layer.biases[biasIndex] -= h;
+                layer.costGradientBias[biasIndex] = deltaCost / h;
+            }
+        }
+
+        ApplyGradients(learnRate);
+    }
+
+    void ApplyGradients(double learnRate)
+    {
+        for (Layer &layer : layers)
+        {
+            layer.ApplyGradients(learnRate);
+        }
+    }
 };
 
-int main(void)
+int main()
 {
-
     NeuralNetwork network({2, 3, 2});
 
-    std::vector<double> input = {1.0, 0.5};
+    std::vector<std::vector<double>> inputs = {
+        {0, 0}, {0, 1}, {1, 0}, {1, 1}};
+    std::vector<std::vector<double>> expectedOutputs = {
+        {1, 0}, {0, 1}, {0, 1}, {1, 0}};
 
-    // Classify & Print
-    int classification = network.Classify(input);
-    std::cout << "Classified as: " << classification << std::endl;
+    int epochs = 1000;
+    double learningRate = 0.1;
 
-    return 0;
+    for (int epoch = 0; epoch < epochs; ++epoch)
+    {
+        network.Learn(inputs, expectedOutputs, learningRate);
+
+        double cost = network.Loss(inputs, expectedOutputs);
+
+        if (epoch % 100 == 0 || epoch == epochs - 1)
+        {
+            std::cout << "Epoch " << epoch << ", Cost: " << cost << std::endl;
+        }
+    }
+
+    std::cout << "\nTesting the trained network:\n";
+    for (size_t i = 0; i < inputs.size(); ++i)
+    {
+        int classification = network.Classify(inputs[i]);
+        std::cout << "Input: [" << inputs[i][0] << ", " << inputs[i][1] << "] ";
+        std::cout << "Classified as: " << classification;
+        std::cout << " (Expected: " << (expectedOutputs[i][0] > expectedOutputs[i][1] ? 0 : 1) << ")\n";
+    }
 }
